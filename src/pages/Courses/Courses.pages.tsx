dc739df9deba081c66c1a1eb730d6839f5c89d7c
majd <amhb371@gmail.com>
2025-04-13 11:10:55 +0330

    test develop

--- src/pages/Courses/Courses.pages.tsx
+++ src/pages/Courses/Courses.pages.tsx
@@ -1,2 +1,31 @@
-const Courses = () => <h1>Courses</h1>;
-export default Courses;
+import React from "react";
+import { Box } from "@mui/material";
+
+import { HeaderLayout } from "layouts";
+import { BreadCrumbsModel } from "types";
+import theme from "theme";
+
+export const DashboardPage: React.FC = () => {
+  const breadcrumbData: BreadCrumbsModel[] = [
+    {
+      title: "دوره",
+      link: "/dashboard",
+      id: "0",
+      color: theme.palette.grey[600],
+      active: true,
+    },
+  ];
+
+  return (
+    <>
+      <HeaderLayout title="دوره ها" breadcrumb={breadcrumbData} />
+      <Box
+        display={"flex"}
+        gap={2}
+        alignItems={"flex-start"}
+        sx={{ flexDirection: { xs: "column-reverse", md: "row", lg: "row" } }}
+        height={"90vh"}
+      ></Box>
+    </>
+  );
+};
